c8a4674f58f59a74e0db32490a5f1ef9deb617d3
Mahammadnazil <mathakiyanazil03@gmail.com>
2026-08-25 13:23:00 +0530

    Que-3

---
+++ p-33_Random_Num_Gen/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  const randomNumber = Math.floor(Math.random() * 100) + 1;
+
+  return (
+    <div>
+      <h1>Random Number: {randomNumber}</h1>
+    </div>
+  );
+}
+
+export default App;
