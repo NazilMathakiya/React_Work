8f9df56c7a3719a57566b98e876e0dabfe7aa0a5
Mahammadnazil <mathakiyanazil03@gmail.com>
2026-08-26 13:47:07 +0530

    Que-37

---
+++ p-37_User_Details/src/App.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+
+function App(){
+  const [users,setUsers]=useState([]);
+  useEffect(()=>{
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response)=>response.json())
+    .then((data)=>{
+      setUsers(data);
+    })
+  },[]);
+
+  return(
+    <div>
+      <h1>User Details</h1>
+
+      {users.map((user)=>(
+        <div key={user.id}>
+          <h2>Name: {user.name}</h2>
+          <p>Email: {user.email}</p>
+          <p>City: {user.address.city}</p>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default App;
