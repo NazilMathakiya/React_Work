ce3bdb90f2e94e16dc5a55a59aa43f406b47fe2c
Mahammadnazil <mathakiyanazil03@gmail.com>
2026-08-26 13:46:03 +0530

    Que-36

---
+++ p-36_Fetch_Users/src/App.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+
+function App(){
+    const [users,setUsers]=useState([]);
+    useEffect(()=>{
+        fetch("https://jsonplaceholder.typicode.com/users")
+        .then((response)=> response.json())
+        .then((data)=>{
+            setUsers(data);
+        });
+    },[]);
+
+    return(
+        <div>
+            <h1>Fetch Users</h1>
+            {users.map((user)=>(
+                <p key={user.id}>{user.name}</p>
+            ))}
+        </div>
+    )
+}
+
+export default App;
