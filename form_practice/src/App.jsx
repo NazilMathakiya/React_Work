bbf3bd2d7b05ad78dbebf6e6b63ddcb8ddccdef7
Mahammadnazil <mathakiyanazil03@gmail.com>
2026-09-01 09:34:35 +0530

    Practice Form

---
+++ form_practice/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+function Form(){
+  const [email,setEmail]=useState("");
+  const [password,setPassword]=useState("");
+  const [isLogin,setIsLogin]=useState(false);
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    setIsLogin(true)
+  }
+
+return(
+  <div>
+      <h1>Form:</h1>
+      <form onSubmit={handleSubmit}>
+      <label>Email:</label>
+      <input type="text" value={email} onChange={(e)=> setEmail(e.target.value)} placeholder="Enter Your Email"/>
+      <label>Password:</label>
+      <input type="text" value={password} onChange={(e)=> setPassword(e.target.value)} placeholder="Enter Your Password"/>
+      <button type="submit">Submit</button>
+
+      </form>
+      {isLogin && <p>Submitted Successfully</p>}
+
+  </div>
+)
+}
+export default Form;
